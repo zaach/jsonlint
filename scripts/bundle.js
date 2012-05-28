3899996991efd47229e5c75794219894db02ad5e
Zachary Carter <zack.carter@gmail.com>
2012-05-28 12:30:05 -0700

    update bundle script

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
-var sys = require('sys');
 
-var source = "var jsonlint = (function(){var require=true,module=false;var exports={};" + 
+var source = "var jsonlint = (function(){var require=true,module=false;var exports={};" +
   fs.readFileSync(__dirname+'/../lib/jsonlint.js', 'utf8') +
   "return exports;})()";
 
-sys.puts(source);
+console.log(source);
 
